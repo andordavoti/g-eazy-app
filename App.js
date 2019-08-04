b9845c3f022cae1147409a4b899c7fa9423a3321
andordavoti <andor.davoti@gmail.com>
2019-08-04 23:10:15 +0200

    expo sdk upgraded to 34

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import { AppLoading } from 'expo';
 import { Asset } from 'expo-asset';
+import * as Font from 'expo-font';
 import React, { useState } from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
 import AppNavigator from './navigation/AppNavigator';
 
 export default function App(props) {
@@ -52,10 +55,17 @@ async function loadResourcesAsync() {
       require('./assets/images/the-tipping-point.jpg'),
       require('./assets/images/the-vault.jpg'),
     ]),
+    Font.loadAsync({
+      // This is the font that we are using for our tab bar
+      ...Ionicons.font,
+      // We include SpaceMono because we use it in HomeScreen.js. Feel free to
+      // remove this if you are not using it in your app
+      'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+    }),
   ]);
 }
 
-function handleLoadingError(error: Error) {
+function handleLoadingError(error) {
   // In this case, you might want to report the error to your error reporting
   // service, for example Sentry
   console.warn(error);
